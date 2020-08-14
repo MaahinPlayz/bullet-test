f61d732e78649bf99fc065a8f33970b4ccce5d03
MaahinPlayz <69353377+MaahinPlayz@users.noreply.github.com>
2020-08-14 14:02:32 -0400

    Add files via upload

---
+++ THIS IS IMPORTAND - Copy/project 20/sketch.js
@@ -0,0 +1,96 @@
+var bullet, wall;
+var speed, weight;
+var thickness;  
+
+
+function setup() {
+  createCanvas(1600 ,400);
+  createSprite(400, 200, 50, 50);
+
+bullet=createSprite(50,200,20,10);
+
+
+
+
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80);
+
+
+
+thickness=random(22,83);
+
+speed=random(223,321);
+weight=random(30,52);
+
+}
+
+
+
+function draw() {
+  background(255,255,255);  
+
+  
+bullet.velocityX=6;
+
+  drawSprites();
+}
+
+
+  
+
+function hasCollided(bullet,wall)
+{
+   
+
+bulletRightEdge=bullet.x + bullet.width
+wallLeftEdge=wall.x;
+
+if (bulletRightEdge>=wallLeftEdge)
+{
+  return true
+}
+  return false;
+}
+
+
+
+
+
+if(hasCollided(bullet,  wall))
+{
+bullet.velocityX=0;
+var damage=0.5 * speed* speed/(thickness *thickness *thickness);
+
+if(damage>10)
+{
+  wall.shapeColor=color(255,0,0);
+}
+
+
+if(damage<10)
+{
+  wall.shapeColor=color(0,255,0);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
